970957248e65b24668c24382037128c7d2408a9a
zangetsu <dev.mehulsaini@gmail.com>
2023-11-26 23:10:26 +0530

    -

--- js/index.js
+++ js/index.js
@@ -65,7 +65,7 @@ function computer_move(){
     let random = Math.random();
     let computer_moves ='';
     if(random >=0 && random < 1/3){
-        document.querySelector('.computer_choice_img').innerHTML = '<img src="https://mehulsaini763.github.io/rock_papaer_scissor/images/rock-emoji.png" width:"100%" height:"100%" />';
+        document.querySelector('.computer_choice_img').innerHTML = '<img src="https://mehulsaini763.github.io/rock_papaer_scissor/images/rock-emoji.jpg" width:"100%" height:"100%" />';
         computer_moves = 'rock';
     }
     else if(random >= 1/3 && random < 2/3){
